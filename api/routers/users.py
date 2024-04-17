e38d98ad44ffd986152e5036c6e4859e97d15e84
Shiran <xiao.shiran@gmail.com>
2024-04-17 14:17:04 -0400

    adding update user api

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Response, HTTPException
 from typing import List, Optional, Union
-from models.users import UserIn, UserOut, UserResponse
+from models.users import UserIn, UserOut, UserResponse, UserUpdate
 from models.errors import Error
 from utils.authentication import hash_password, try_get_jwt_user_data
 from queries.user_queries import (
@@ -26,6 +26,23 @@ def create_user(
 def get_user(
     user_id: int,
     repo: UserRepository = Depends(),
-    User: UserResponse = Depends(try_get_jwt_user_data),
+    user_response: UserResponse = Depends(try_get_jwt_user_data),
 ) -> UserOut:
+    if user_response is None:
+        raise HTTPException(status_code=401, detail='You must login!')
     return repo.get_user(user_id)
+
+
+@router.put("/patients/{user_id}", response_model=Union[UserOut, Error])
+def update_user(
+    user_id: int,
+    user: UserUpdate,
+    repo: UserRepository = Depends(),
+    user_response: UserResponse = Depends(try_get_jwt_user_data),
+) -> Union[UserOut, Error]:
+    if user_response is None:
+        raise HTTPException(status_code=401, detail='You must login!')
+    if user.password != user.confirmed_password:
+        raise HTTPException(status_code=422, detail="Passwords do not match")
+    hashed_password = hash_password(user.password)
+    return repo.update_user(user_id, user, hashed_password)
