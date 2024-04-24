ee3b958abd8de983a62dca247726dd0da5023048
genessy <genessymnz@gmail.com>
2024-04-24 14:22:31 -0400

    Gutted out any useless code

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import ErrorNotification from './components/ErrorNotification'
-import Construct from './components/Construct'
 
 import './App.css'
 
@@ -24,41 +23,10 @@ if (!API_HOST) {
  * @returns {React.ReactNode}
  */
 function App() {
-    // Replace this App component with your own.
-    /** @type {[LaunchInfo | undefined, (info: LaunchInfo) => void]} */
-    const [launchInfo, setLaunchInfo] = useState()
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        async function getData() {
-            let url = `${API_HOST}/api/launch-details`
-            console.log('fastapi url: ', url)
-            let response = await fetch(url)
-            /** @type {LaunchData} */
-            let data = await response.json()
-
-            if (response.ok) {
-                if (!data.launch_details) {
-                    console.log('drat! no launch data')
-                    setError('No launch data')
-                    return
-                }
-                console.log('got launch data!')
-                setLaunchInfo(data.launch_details)
-            } else {
-                console.log('drat! something happened')
-                setError(data.message)
-            }
-        }
-        getData()
-    }, [])
-
     return (
         <div className="App">
             <header className="App-header">{/* <Nav /> */}</header>
-            <Outlet />
-            <ErrorNotification error={error} />
-            <Construct info={launchInfo} />
+            {/* <ErrorNotification error={error} /> */}
         </div>
     )
 }
--- ghi/src/components/SignInForm.jsx
+++ ghi/src/components/SignInForm.jsx
@@ -1,30 +1,20 @@
 // @ts-check
 import { useState } from 'react'
-import { Navigate } from 'react-router-dom'
-
-import useAuthService from '../hooks/useAuthService'
 
 export default function SignInForm() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const { signin, user, error } = useAuthService()
 
     /**
      * @param {React.FormEvent<HTMLFormElement>} e
      */
     async function handleFormSubmit(e) {
         e.preventDefault()
-        await signin({ username, password })
-    }
-
-    if (user) {
-        console.log('user', user)
-        return <Navigate to="/" />
+        console.log("handleFormSubmit")
     }
 
     return (
         <form onSubmit={handleFormSubmit}>
-            {error && <div className="error">{error.message}</div>}
 
             <input
                 type="text"
