3fab1ec3fc0713ab6fe48219f9074c21724f779d
Sean Burch <seanburch05@gmail.com>
2024-05-09 19:40:21 +0000

    updated resdetail css/style, fixed buttons

--- ghi/src/components/ReservationDetail.jsx
+++ ghi/src/components/ReservationDetail.jsx
@@ -4,7 +4,7 @@ import ErrorNotification from './ErrorNotification'
 import {useEffect } from 'react'
 import SideNav from './SideNav'
 
-const ReservationDetail = () => {
+const reservationDetail = () => {
     const { id } = useParams()
     const { data, isLoading, error } = useReservationDetailQuery(id)
     const navigate = useNavigate()
@@ -41,87 +41,74 @@ const ReservationDetail = () => {
         const currentTime = new Date(data.date+"T"+data.time)
         const time = currentTime.toLocaleTimeString()
         return (
-            <div className="container-fluid h-100">
-            <div className="row h-100">
-                <SideNav/>
-                    <div className="col-10">
-                        <div>
-                            <h1 className="mt-3">Reservation Detail</h1>
-                        </div>
-                        <div>
-                            <table className="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Date</th>
-                                        <th>Time</th>
-                                        <th>Doctor</th>
-                                        <th>Reason</th>
-                                        <th>Insurance</th>
-                                        <th>
-                                            <NavLink
-                                                className="nav-link active"
-                                                aria-current="page"
-                                                to={`/reservations/new/`}
-                                            >
-                                                <button variant="primary">
-                                                    Create New Reservation
-                                                </button>
-                                            </NavLink>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr key={data.id}>
-                                        <td>{date}</td>
-                                        <td>{time}</td>
-                                        <td>
-                                            {data.first_name} {data.last_name}
-                                        </td>
-                                        <td>{data.reason}</td>
-                                        <td>{data.insurance}</td>
-                                        <td>
-                                            <button
-                                                onClick={() =>
-                                                    navigate(
-                                                        `/reservations/${data.id}/update`
-                                                    )
-                                                }
-                                            >
-                                                Update
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                onClick={() =>
-                                                    cancelReservation(data.id)
-                                                }
-                                            >
-                                                Cancel
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                onClick={() =>
-                                                    completeReservation(data.id)
-                                                }
-                                            >
-                                                Complete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+            <div>
+                <br />
+                <br />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="">
+                                <div className="table-responsive">
+                                    <h1 className="crtitle">Reservation Detail</h1>
+                                    <br/>
+                                    <table className="table project-list-table table-nowrap align-middle table-borderless">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Doctor</th>
+                                                <th scope="col">Date</th>
+                                                <th scope="col">Time</th>
+                                                <th scope="col">Reason</th>
+                                                <th scope="col">Insurance</th>
+                                                <th scope="col">&nbsp;&nbsp;&nbsp;&nbsp;Action</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                                        <tr key={data.id}>
+                                                                <td><img src={data.image} alt="" className="avatar-sm rounded-circle me-2" /><a href="#" className="text-body">{data.first_name} {data.last_name}</a></td>
+                                                                <td>{date}</td>
+                                                                <td>{time}</td>
+                                                                <td><span className="badge badge-soft-success mb-0">{data.reason}</span></td>
+                                                                <td>{data.insurance}</td>
+                                                                <td>
+                                                                    <ul className="list-inline mb-0">
+                                                                        <li className="list-inline-item">
+                                                                            <button type="button" className="btn complete" data-bs-toggle="button"
+                                                                                onClick={() =>
+                                                                                    cancelReservation(data.id)
+                                                                                }
+                                                                            >
+                                                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                                                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                                                                                </svg>
+                                                                            </button>
+                                                                        </li>
+                                                                        <li className="list-inline-item">
+                                                                            <button type="button" className="btn cancel" data-bs-toggle="button"
+                                                                                onClick={() =>
+                                                                                    completeReservation(data.id)
+                                                                                }
+                                                                            >
+                                                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                                                                                </svg>
+                                                                            </button>
+                                                                        </li>
+                                                                    </ul>
+                                                                </td>
+                                                            </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <ErrorNotification error={error} />
-            </div>
-        )
+
+
+
+    )} else {
+        return <ErrorNotification error={error} />;
     }
 }
-export default ReservationDetail
+export default reservationDetail
